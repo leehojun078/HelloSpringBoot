11037a51e0da4728bae2a6062bc1379ffcf2c3ce
leehojun078 <rainbow_83@naver.com>
2018-06-06 22:03:26 +0900

    Create new project for helloSpringBoot

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into customer(firstname, lastname) value ("alice", "kim")
+insert into customer(firstname, lastname) value ("bob", "kim")
+insert into customer(firstname, lastname) value ("chalie", "lee")
